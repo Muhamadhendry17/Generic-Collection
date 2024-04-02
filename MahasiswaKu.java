f2cdcc29459aff9dff903d9508f5cc06216380a7
rahmanrizaluddin <hendriramadhan06@gmail.com>
2024-04-02 12:25:53 +0700

    first commit

---
+++ MahasiswaKu.java
@@ -0,0 +1,40 @@
+public class MahasiswaKu<T, U, V> {
+    private T nim;
+    private U name;
+    private V clas;
+
+    public T getNim() {
+        return nim;
+    }
+
+    public void setNim(T nim) {
+        this.nim = nim;
+    }
+
+    public U getName() {
+        return name;
+    }
+
+    public void setName(U name) {
+        this.name = name;
+    }
+
+    public V getClas() {
+        return clas;
+    }
+
+    public void setClas(V clas) {
+        this.clas = clas;
+    }
+
+    public static void main(String[] args) {
+        MahasiswaKu<String, String, Integer> m = new MahasiswaKu<>();
+        m.setNim("1102020");
+        m.setName("Ferdi");
+        m.setClas(21);
+
+        System.out.println(m.getNim());
+        System.out.println(m.getName());
+        System.out.println(m.getClas());
+    }
+}
